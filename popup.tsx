23d6bc75e6a403fc8d14b82a8b199211fcf5a098
flaviokicis <flavouredsoda@gmail.com>
2022-10-21 16:26:02 -0300

    Fix: a

--- popup.tsx
+++ popup.tsx
@@ -26,7 +26,7 @@ function IndexPopup() {
       <button
         onClick={toggleActive}
         className="inline-flex items-center flex-1 p-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        {active ? "Deactivate" : "Activate"}
+        {active ? "Deactivate" : "Activated"}
       </button>
     </div>
   )
